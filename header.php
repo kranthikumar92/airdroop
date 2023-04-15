830468941ac63c5aba112fd835ee7dca123f24d6
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-15 22:55:52 +0530

    check

---
+++ header.php
@@ -0,0 +1,114 @@
+<!--== Search Box Area Start ==-->
+<div class="search-box-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8  m-auto">
+                <div class="search-form-wrap">
+                    <form action="#" method="post">
+                        <input type="search" placeholder="type keyword and hit enter" />
+                        <i class="cursor"></i>
+                        <button class="btn sr-only"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="search-close-btn">
+        <a href="#" class="close-btn search-close-btn"><img src="assets/img/icons/cancel.png" alt="Cancel" /></a>
+    </div>
+</div>
+<!--== Search Box Area End ==-->
+
+<!-- Login Modal Start -->
+<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Welcome Back !</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="login-content-wrap airdrop-form">
+                    <form action="#" method="post">
+                        <div class="single-input-item">
+                            <input type="text" placeholder="Username"/>
+                        </div>
+
+                        <div class="single-input-item">
+                            <input type="password" placeholder="Password"/>
+                        </div>
+
+                        <div class="single-input-item">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input"
+                                       id="remember">
+                                <label class="custom-control-label" for="remember">Remember Me</label>
+                            </div>
+                        </div>
+
+                        <div class="single-input-item">
+                            <button class="btn btn-gradiant">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Login Modal End -->
+
+<!--== Header Area Start ==-->
+<header id="header-area">
+    <!-- Preheader Area Start -->
+    <div class="preheader-area d-none d-lg-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="preheader-content-wrap d-flex">
+                        <img src="assets/img/images/pre-ads-logo.png" alt="Ads Logo"/>
+                        <h2 class="h5">JOIN WITH US & GET <span>20% - 50% BONUS</span></h2>
+                        <a href="#" class="btn btn-green">Join</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Preheader Area End -->
+
+    <!-- Navigation Area Start -->
+    <div class="header-navigation" id="fixheader">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-xl">
+                        <a class="navbar-brand" href="index-2.php">
+                            <img src="assets/img/logo.png" alt="Logo" />
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"  data-target="#mainMenu">
+                            <span class="navbar-toggler-icon"><i class="fa fa-bars"></i></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="mainMenu">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="current"><a href="index-2.php">Home</a></li>
+                                <li><a href="airdrop-listing.php">Airdrops</a></li>
+                                <li><a href="submit.php">Submit Airdrop</a></li>
+                                <li><a href="single-company.php">Company Profile</a></li>
+                                <li><a href="news.php">News</a></li>
+                            </ul>
+                            <div class="member-area">
+                                <a href="#" class="btn btn-brand" data-toggle="modal" data-target="#loginModal" role="button">Login</a>
+                                <a href="step.php" class="btn btn-blue">Singup</a>
+                                <a href="#" class="btn btn-round btn-search"><i class="fa fa-search"></i></a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Navigation Area End -->
+</header>
+<!--== Header Area End ==-->
