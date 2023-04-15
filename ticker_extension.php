03d75f9bfa6d157cccb7d83b8082fa3ac17bc8ad
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-16 00:19:55 +0530

    check

---
+++ ticker_extension.php
@@ -0,0 +1,99 @@
+<div id="currency-rate-area">
+    <div class="currency-rate-slider">
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-up">(+5.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a  href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-up">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-up">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-up">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-up">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-up">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+
+        <!-- Single Currency Rate Start -->
+        <a href="single-airdrop.php" class="single-currency-rate">
+            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
+        </a>
+        <!-- Single Currency Rate End -->
+    </div>
+</div>
