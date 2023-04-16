e2c4583ce6ca3dd6398131a69fe2b58b81363814
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-16 19:03:39 +0530

    check

--- airdrop-listing.php
+++ airdrop-listing.php
@@ -272,7 +272,7 @@ $statusClass = ($row['status'] == 'active') ? 'popular-tag' : 'popular-tag-end';
     </div>
 
 
-    <h2 class="pinpost"><a href="airdrop-listing.php" class="<?php echo $statusClass; ?>"><?php echo $row['status']; ?></a></h2>
+    <h2 class="pinpost"><?php echo $row['status']; ?></h2>
 </div>
 
 <?php
