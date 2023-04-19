5cbad8bc0e0fac38cd7343a1e91a8127fb2f8c7c
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-19 18:21:13 +0530

    check

--- header.php
+++ header.php
@@ -19,21 +19,6 @@
 </div>
 <!--== Search Box Area End ==-->
 
-<!-- Login Modal Start -->
-<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title">Welcome Back !</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Login Modal End -->
-
 <!--== Header Area Start ==-->
 <header id="header-area">
     <!-- Preheader Area Start -->
