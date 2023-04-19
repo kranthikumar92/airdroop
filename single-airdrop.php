0ce4057507c39270c0c8bcb3b9c51734e64c32a1
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-19 11:41:42 +0530

    check

--- single-airdrop.php
+++ single-airdrop.php
@@ -168,11 +168,11 @@ if ($result->num_rows > 0) {
             <ul class="giveway-list">
                 <li><span><img src="assets/img/icons/giveway/coin.png" alt="Coin"/>Blockchain</span>: ' . $row["blockchain"] . '</li>
                 <li><span><img src="assets/img/icons/giveway/doller.png" alt="Coin"/>Est Value</span>: ' . $row["est_value"] . '</li>
-                <li><span><img src="assets/img/icons/giveway/check.png" alt="Coin"/>Tokens</span>: ' . $row["tokens"] . '</li>
+                <li><span><img src="assets/img/icons/giveway/total.png" alt="Coin"/>Tokens</span>: ' . $row["tokens"] . '</li>
                 <li><span><img src="assets/img/icons/giveway/alerm.png" alt="Coin"/>End Date</span>: ' . $row["end_date"] . '</li>
-                <li><span><img src="assets/img/icons/giveway/total.png" alt="Coin"/>Referral</span>: ' . $row["referral_available"] . '</li>
+                <li><span><img src="assets/img/icons/giveway/person.png" alt="Coin"/>Referral</span>: ' . $row["referral_available"] . '</li>
                 <li><span><img src="assets/img/icons/giveway/person.png" alt="Coin"/>Published on</span>: ' . $row["publish_date"] . '</li>
-                <li><span><img src="assets/img/icons/giveway/flag.png" alt="Coin"/>Difficulty</span>: ' . $row["status"] . '</li>
+                <li><span><img src="assets/img/icons/giveway/check.png" alt="Coin"/>Status</span>: ' . $row["status"] . '</li>
             </ul>
           </div>  
           ';
