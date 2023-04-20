e03bfd919305ea56de085a28b507a23480764227
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-20 18:34:24 +0530

    check

--- admin/add-airdrop.php
+++ admin/add-airdrop.php
@@ -204,7 +204,7 @@
                 </div>
 
                 <div class="form-group">
-                  
+                  <label class="col-sm-2 control-label form-label"></label>
                   <div class="col-sm-10">
                     <button type="submit" class="btn btn-default">Submit</button>
                   </div>
