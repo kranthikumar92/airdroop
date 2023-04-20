5eb87349b41f1cb19d820a2cec11b5301a9f849e
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-20 19:04:12 +0530

    check

--- admin/add-airdrop.php
+++ admin/add-airdrop.php
@@ -204,7 +204,7 @@
                 </div>
 
                 <div class="form-group">
-                  <label for="file">Select image to upload:</label>
+                  <label for="file" class="col-sm-2 control-label form-label">Select image to upload:</label>
                   <div class="col-sm-10">
                     <input type="file" name="file" id="file"><br>
                   </div>
