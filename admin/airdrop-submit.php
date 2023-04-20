026c9aee9ba1fec8ac957c7ef096f222c730b4f6
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-21 00:15:20 +0530

    check

--- admin/airdrop-submit.php
+++ admin/airdrop-submit.php
@@ -82,10 +82,6 @@
             <div class="form-group">
             <?php
 
-// Enable error reporting
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 // Database connection parameters
 $servername = "localhost";
