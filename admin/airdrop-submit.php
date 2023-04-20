5b3a7b9c3aabc9c1bafaeaee0e1db4cd997d9cf0
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-21 00:08:29 +0530

    check

--- admin/airdrop-submit.php
+++ admin/airdrop-submit.php
@@ -131,7 +131,7 @@ mysqli_close($conn);
 ?>
 
         <div class="form-group">
-                  <label class="col-sm-2 control-label form-label"></label>
+                  
                   <div class="col-sm-10">
                     <button type="submit" class="btn btn-default">Submit</button>
                   </div>
