0a2f876c2791f102660acfccbbf6fd404c265a22
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-20 20:53:33 +0530

    check

--- admin/airdrop-submit.php
+++ admin/airdrop-submit.php
@@ -1,4 +1,9 @@
 <?php
+
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
         // Database connection parameters
 $servername = "localhost";
 $username = "calix_web_user";
