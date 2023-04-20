93a8b4453f4c9389222596db4453c2f117ce8803
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-20 16:29:01 +0530

    check

--- admin/add-airdrop.php
+++ admin/add-airdrop.php
@@ -79,13 +79,6 @@
     <div class="col-md-12">
       <div class="panel panel-default">
 
-        <div class="panel-title">
-          <ul class="panel-tools">
-            <li><a class="icon minimise-tool"><i class="fa fa-minus"></i></a></li>
-            <li><a class="icon expand-tool"><i class="fa fa-expand"></i></a></li>
-            <li><a class="icon closed-tool"><i class="fa fa-times"></i></a></li>
-          </ul>
-        </div>
 
             <div class="panel-body">
               <form class="form-horizontal">
