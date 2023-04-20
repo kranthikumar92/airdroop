0936941f0d6da0fa41c333f34e389819ae61e72f
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-20 16:19:58 +0530

    check

--- admin/main-nav.php
+++ admin/main-nav.php
@@ -31,16 +31,12 @@
 <ul class="sidebar-panel nav">
   <li class="sidetitle">MORE</li>
   <li><a href="grid.html"><span class="icon color15"><i class="fa fa-columns"></i></span>Profile</a></li>
-  <li><a href="maps.html"><span class="icon color7"><i class="fa fa-map-marker"></i></span>Menu 2</a></li>
+  <li><a href="maps.html"><span class="icon color7"><i class="fa fa-map-marker"></i></span>Users</a></li>
 </ul>
 
 <div class="sidebar-plan">
-  Pro Plan<a href="#" class="link">Upgrade</a>
-  <div class="progress">
-  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-  </div>
-</div>
-<span class="space">42 GB / 100 GB</span>
+  Logout<a href="#" class="link">Click Here</a>
+  
 </div>
 
 </div>
