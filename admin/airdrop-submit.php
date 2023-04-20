924f2aab9730797acdb0d175ddb88f9c07d40320
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-21 00:17:06 +0530

    check

--- admin/airdrop-submit.php
+++ admin/airdrop-submit.php
@@ -146,16 +146,11 @@ mysqli_close($conn);
  <!-- //////////////////////////////////////////////////////////////////////////// --> 
 
 
-<!-- Start Footer -->
-<div class="row footer">
-  <div class="col-md-6 text-left">
-  Copyright © 2015 <a href="http://themeforest.net/user/egemem/portfolio" target="_blank">Egemem</a> All rights reserved.
-  </div>
-  <div class="col-md-6 text-right">
-    Design and Developed by <a href="http://themeforest.net/user/egemem/portfolio" target="_blank">Egemem</a>
-  </div> 
-</div>
-<!-- End Footer -->
+<!--== Footer Start ==-->
+<?php
+        include('footer.php');
+    ?>
+    <!--== Footer End ==-->
 
 
 </div>
