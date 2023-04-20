1259e2425cc25ea69d6d6d057cb464a35a0fefe9
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-20 16:15:24 +0530

    check

--- admin/main-nav.php
+++ admin/main-nav.php
@@ -4,28 +4,26 @@
 <ul class="sidebar-panel nav">
   <li class="sidetitle">MAIN</li>
   <li><a href="index.html"><span class="icon color5"><i class="fa fa-home"></i></span>Dashboard<span class="label label-default">2</span></a></li>
-  <li><a href="mailbox.html"><span class="icon color6"><i class="fa fa-envelope-o"></i></span>Mailbox<span class="label label-default">19</span></a></li>
+  <li><a href="mailbox.html"><span class="icon color6"><i class="fa fa-envelope-o"></i></span>Contact Messages<span class="label label-default">19</span></a></li>
   <li><a href="#"><span class="icon color7"><i class="fa fa-flask"></i></span>Airdrop<span class="caret"></span></a>
     <ul>
       <li><a href="icons.html">Airdrops List</a></li>
       <li><a href="tabs.html">Add New Airdrop</a></li>
     </ul>
   </li>
-  <li><a href="charts.html"><span class="icon color8"><i class="fa fa-bar-chart"></i></span>Charts</a></li>
-  <li><a href="#"><span class="icon color9"><i class="fa fa-th"></i></span>Tables<span class="caret"></span></a>
+  <li><a href="#"><span class="icon color9"><i class="fa fa-th"></i></span>Blog<span class="caret"></span></a>
     <ul>
-      <li><a href="basic-table.html">Basic Tables</a></li>
-      <li><a href="data-table.html">Data Tables</a></li>
+      <li><a href="basic-table.html">Posts List</a></li>
+      <li><a href="data-table.html">Add New Post</a></li>
     </ul>
   </li>
-  <li><a href="#"><span class="icon color10"><i class="fa fa-check-square-o"></i></span>Forms<span class="caret"></span></a>
+  <li><a href="#"><span class="icon color10"><i class="fa fa-check-square-o"></i></span>Information<span class="caret"></span></a>
     <ul>
-      <li><a href="form-elements.html">Form Elements</a></li>
-      <li><a href="layouts.html">Form Layouts</a></li>
-      <li><a href="text-editors.html">Text Editors</a></li>
+      <li><a href="form-elements.html">All Pages</a></li>
+      <li><a href="layouts.html">Add New Page</a></li>
     </ul>
   </li>
-  <li><a href="widgets.html"><span class="icon color11"><i class="fa fa-diamond"></i></span>Widgets</a></li>
+  <li><a href="widgets.html"><span class="icon color14"><i class="fa fa-paper-plane-o"></i></span>Airdrop Requests</a></li>
   <li><a href="calendar.html"><span class="icon color8"><i class="fa fa-calendar-o"></i></span>Calendar<span class="label label-danger">NEW</span></a></li>
   <li><a href="typography.html"><span class="icon color12"><i class="fa fa-font"></i></span>Typography</a></li>
   <li><a href="#"><span class="icon color14"><i class="fa fa-paper-plane-o"></i></span>Extra Pages<span class="caret"></span></a>
