ab9a2581000f6d0d932cf4867d992f3e4231aca6
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-22 00:34:31 +0530

    update to code

--- admin/edit-airdrop.php
+++ admin/edit-airdrop.php
@@ -100,10 +100,10 @@ if ($conn->connect_error) {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-
 // Define the directory where the images will be stored
   $target_dir = "../airdrop_imgs/";
   
+
   // Get the name of the uploaded file
   $original_filename = basename($_FILES["file"]["name"]);
   $file_extension = pathinfo($original_filename, PATHINFO_EXTENSION);
@@ -113,6 +113,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // Move the uploaded file to the target directory
   if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
 
+    $publist_date = $_POST['publish_date'];
+    $step_bystep = mysqli_real_escape_string($conn, $_POST['step_by_step_guide']);
   // Prepare the SQL statement
   $sql1 = "UPDATE airdrop_coins SET coin_name='".$_POST['airdrop_title']."', coin_img='".$new_filename."', coin_tagline='".$_POST['airdrop_tagline']."', airdrop_description='".$_POST['airdrop_description']."', airdrop_steps='".$step_bystep."', whitepaper='".$_POST['whitepaper']."', tokens='".$_POST['tokens']."', est_value='".$_POST['est_value']."', end_date='".$_POST['end_date']."', referral_available='".$_POST['referral_available']."', referral_link='".$_POST['referral_link']."', blockchain='".$_POST['blockchain']."', website_link='".$_POST['website_link']."', airdrop_join_link='".$_POST['airdrop_join_link']."', publish_date='".$publist_date."', status='".$_POST['status']."' WHERE sno='".$update_id."'";
 
