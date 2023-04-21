d577ccc144b547a3c6ca02155a4e7c4c6bedff9e
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-21 11:33:42 +0530

    check

---
+++ admin/blog-list.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Kode is a Premium Bootstrap Admin Template, It's responsive, clean coded and mobile friendly">
+  <meta name="keywords" content="bootstrap, admin, dashboard, flat admin template, responsive," />
+  <title>Kode - Premium Bootstrap Admin Template</title>
+
+  <!-- ========== Css Files ========== -->
+  <link href="css/root.css" rel="stylesheet">
+
+  </head>
+  <body>
+  <!-- Start Page Loading -->
+  <div class="loading"><img src="img/loading.gif" alt="loading-img"></div>
+  <!-- End Page Loading -->
+ <!-- //////////////////////////////////////////////////////////////////////////// --> 
+  <!-- START TOP -->
+  <div id="top" class="clearfix">
+
+  	<!-- Start App Logo -->
+  	<div class="applogo">
+  		<a href="index.html" class="logo">kode</a>
+  	</div>
+  	<!-- End App Logo -->
+
+    <!-- Start Sidebar Show Hide Button -->
+    <a href="#" class="sidebar-open-button"><i class="fa fa-bars"></i></a>
+    <a href="#" class="sidebar-open-button-mobile"><i class="fa fa-bars"></i></a>
+    <!-- End Sidebar Show Hide Button -->
+
+    <!--== Top Nav Start ==-->
+    <?php
+        include('top-nav.php');
+    ?>
+    <!--== Top Nav End ==-->
+
+  </div>
+  <!-- END TOP -->
+ <!-- //////////////////////////////////////////////////////////////////////////// --> 
+
+
+<!-- //////////////////////////////////////////////////////////////////////////// --> 
+    <!--== Main Nav Start ==-->
+    <?php
+        include('main-nav.php');
+    ?>
+    <!--== Main Nav End ==-->
+<!-- //////////////////////////////////////////////////////////////////////////// --> 
+
+ <!-- //////////////////////////////////////////////////////////////////////////// --> 
+<!-- START CONTENT -->
+<div class="content">
+
+  <!-- Start Page Header -->
+  <div class="page-header">
+    <h1 class="title">Crypto Airdrops Listing</h1>
+      <ol class="breadcrumb">
+        <li><a href="airdrops.php">Airdrop</a></li>
+        <li class="active">Airdrops List</li>
+      </ol>
+
+  </div>
+  <!-- End Page Header -->
+
+
+ <!-- //////////////////////////////////////////////////////////////////////////// --> 
+<!-- START CONTAINER -->
+<div class="container-padding">
+
+
+  <!-- Start Row -->
+  <div class="row">
+
+    <!-- Start Panel -->
+    <div class="col-md-12">
+      <div class="panel panel-default">
+        <div class="panel-body table-responsive">
+
+        <?php
+// Database connection parameters
+$servername = "localhost";
+$username = "calix_web_user";
+$password = "calixworldhhUUh383287HGSHhs";
+$dbname = "calix_cry_world";
+
+// Create database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve data from the airdrop_coins table
+$sql = "SELECT * FROM airdrop_coins";
+$result = $conn->query($sql);
+
+// Create a table with the retrieved data
+if ($result->num_rows > 0) {
+    echo "<table id='airdrop-listing' class='table display'>";
+    echo "<thead><tr><th>sno</th><th>Coin Name</th><th>Est Value</th><th>Referral available</th><th>End date</th><th>Status</th><th>Action</th></tr></thead>";
+    echo "<tfoot><tr><th>sno</th><th>Coin Name</th><th>Est Value</th><th>Referral available</th><th>End date</th><th>Status</th><th>Action</th></tr></tfoot>";
+    echo "<tbody>";
+
+// Determine the CSS class for status
+
+
+    while ($row = $result->fetch_assoc()) {
+        $statusClass = ($row['status'] == 'active') ? 'green' : 'red';
+        echo "<tr>";
+        echo "<td>" . $row["sno"] . "</td>";
+        echo "<td>" . $row["coin_name"] . "</td>";
+        echo "<td>" . $row["est_value"] . "</td>";
+        echo "<td>" . $row["referral_available"] . "</td>";
+        echo "<td>" . $row["end_date"] . "</td>";
+        echo "<td  style='color:" . $statusClass . "'>" . $row["status"] . "</td>";
+        echo "<td><a href='edit-airdrop.php?id=" . $row["sno"] . "' class='btn btn-warning'>Edit</a> <a href='delete-airdrop.php?id=" . $row["sno"] . "' class='btn btn-danger'>Delete</a></td>";
+        echo "</tr>";
+    }
+
+    echo "</tbody></table>";
+} else {
+    echo "No data found.";
+}
+
+// Close database connection
+$conn->close();
+?>
+
+
+
+        </div>
+
+      </div>
+    </div>
+    <!-- End Panel -->
+
+
+  </div>
+  <!-- End Row -->
+
+</div>
+<!-- END CONTAINER -->
+ <!-- //////////////////////////////////////////////////////////////////////////// --> 
+
+
+<!--== Footer Start ==-->
+<?php
+        include('footer.php');
+    ?>
+<!--== Footer End ==-->
+
+
+</div>
+<!-- End Content -->
+ <!-- //////////////////////////////////////////////////////////////////////////// --> 
+
+
+
+
+
+<!-- ================================================
+jQuery Library
+================================================ -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+
+<!-- ================================================
+Bootstrap Core JavaScript File
+================================================ -->
+<script src="js/bootstrap/bootstrap.min.js"></script>
+
+<!-- ================================================
+Plugin.js - Some Specific JS codes for Plugin Settings
+================================================ -->
+<script type="text/javascript" src="js/plugins.js"></script>
+
+<!-- ================================================
+Data Tables
+================================================ -->
+<script src="js/datatables/datatables.min.js"></script>
+
+
+
+<script>
+$(document).ready(function() {
+    $('#airdrop-listing').DataTable();
+} );
+</script>
+
+
+
+<script>
+$(document).ready(function() {
+    var table = $('#example').DataTable({
+        "columnDefs": [
+            { "visible": false, "targets": 2 }
+        ],
+        "order": [[ 2, 'asc' ]],
+        "displayLength": 25,
+        "drawCallback": function ( settings ) {
+            var api = this.api();
+            var rows = api.rows( {page:'current'} ).nodes();
+            var last=null;
+ 
+            api.column(2, {page:'current'} ).data().each( function ( group, i ) {
+                if ( last !== group ) {
+                    $(rows).eq( i ).before(
+                        '<tr class="group"><td colspan="5">'+group+'</td></tr>'
+                    );
+ 
+                    last = group;
+                }
+            } );
+        }
+    } );
+ 
+    // Order by the grouping
+    $('#example tbody').on( 'click', 'tr.group', function () {
+        var currentOrder = table.order()[0];
+        if ( currentOrder[0] === 2 && currentOrder[1] === 'asc' ) {
+            table.order( [ 2, 'desc' ] ).draw();
+        }
+        else {
+            table.order( [ 2, 'asc' ] ).draw();
+        }
+    } );
+} );
+</script>
+
+
+</body>
+</html>
