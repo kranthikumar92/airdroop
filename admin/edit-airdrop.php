65cc12f8a6fc60f3eb40a230c2122e86389fb4e7
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-23 07:44:24 +0530

    update to code

--- admin/edit-airdrop.php
+++ admin/edit-airdrop.php
@@ -312,7 +312,7 @@ if (mysqli_num_rows($result) > 0) {
                 <div class="form-group">
                   <label class="col-sm-2 control-label form-label"></label>
                   <div class="col-sm-10">
-                    <button type="submit" class="btn btn-default">Submit</button>
+                    <button type="submit" class="btn btn-default">Submit</button> <a href="airdrops.php" class="btn btn-default">Back</a>
                   </div>
                 </div>
                 
