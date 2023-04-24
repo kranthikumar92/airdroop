603e81d02b493ca967e0a6d435fccd11bcdf56e6
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-24 19:01:07 +0530

    update to code

--- admin/main-nav.php
+++ admin/main-nav.php
@@ -5,33 +5,33 @@
   <li class="sidetitle">MAIN</li>
   <li><a href="index.html"><span class="icon color5"><i class="fa fa-home"></i></span>Dashboard<span class="label label-default"></span></a></li>
   <li><a href="contact-messages.php"><span class="icon color6"><i class="fa fa-envelope-o"></i></span>Contact Messages<span class="label label-default"></span></a></li>
-  <li><a href="#"><span class="icon color7"><i class="fa fa-flask"></i></span>Airdrop<span class="caret"></span></a>
+  <li><a href="#"><span class="icon color7"><i class="fa-rocket"></i></span>Airdrop<span class="caret"></span></a>
     <ul>
       <li><a href="airdrops.php">Airdrops List</a></li>
       <li><a href="add-airdrop.php">Add New Airdrop</a></li>
     </ul>
   </li>
-  <li><a href="#"><span class="icon color9"><i class="fa fa-th"></i></span>Blog<span class="caret"></span></a>
+  <li><a href="#"><span class="icon color9"><i class="fa-rss-square"></i></span>Blog<span class="caret"></span></a>
     <ul>
       <li><a href="blog-list.php">Posts List</a></li>
       <li><a href="add-blog.php">Add New Post</a></li>
     </ul>
   </li>
-  <li><a href="#"><span class="icon color10"><i class="fa fa-check-square-o"></i></span>Information<span class="caret"></span></a>
+  <li><a href="#"><span class="icon color10"><i class="fa-info"></i></span>Information<span class="caret"></span></a>
     <ul>
       <li><a href="info-pages.php">All Pages</a></li>
       <li><a href="add-info-page.php">Add New Page</a></li>
     </ul>
   </li>
-  <li><a href="airdrop-requests.php"><span class="icon color14"><i class="fa fa-paper-plane-o"></i></span>Airdrop Requests</a></li>
-  <li><a href="#"><span class="icon color8"><i class="fa fa-calendar-o"></i></span>Ads Corner<span class="label label-danger">NEW</span></a></li>
+  <li><a href="airdrop-requests.php"><span class="icon color14"><i class="fa-futbol-o"></i></span>Airdrop Requests</a></li>
+  <li><a href="#"><span class="icon color8"><i class="fa-gears"></i></span>Ads Corner<span class="label label-danger">NEW</span></a></li>
   
 </ul>
 
 <ul class="sidebar-panel nav">
   <li class="sidetitle">MORE</li>
-  <li><a href="users.php"><span class="icon color15"><i class="fa fa-columns"></i></span>Users</a></li>
-  <li><a href="add-user.php"><span class="icon color7"><i class="fa fa-map-marker"></i></span>Add User</a></li>
+  <li><a href="users.php"><span class="icon color15"><i class="fa-users"></i></span>Users</a></li>
+  <li><a href="add-user.php"><span class="icon color7"><i class="fa-user"></i></span>Add User</a></li>
 </ul>
 
 <div class="sidebar-plan">
