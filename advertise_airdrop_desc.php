9d301eb4d04bcdb8e88b19b2f31aaf8f1d24d5d8
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-24 23:46:56 +0530

    update to code

--- advertise_airdrop_desc.php
+++ advertise_airdrop_desc.php
@@ -1,1 +1,30 @@
-<a href="#"><img src="assets/img/images/listing-advise.jpg" alt="advisement" class="img-fluid"/></a>
+<!--Retrieve data from db -->
+<?php
+    // Establish a database connection
+    $servername = "localhost";
+    $username = "calix_web_user";
+    $password = "calixworldhhUUh383287HGSHhs";
+    $dbname = "calix_cry_world";
+
+    // Create database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Query to retrieve blog post with ID 1
+$sql = "SELECT * FROM advertisement WHERE ads_location = 'airdrop_desc' AND status = 'Active'";
+$result = mysqli_query($conn, $sql);
+
+// Check if the query returned any rows
+if (mysqli_num_rows($result) > 0) {
+    // Fetch the first row as an associative array
+    $post = mysqli_fetch_assoc($result);
+
+?>
+<a href="<?php echo $post['banner_link']; ?>" class="d-block"><img src="ads_imgs/<?php echo $post['banner_img']; ?>" class="img-fluid" alt="<?php echo $post['banner_title']; ?>"/></a>
+<?php
+}
+?>
