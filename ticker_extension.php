15751817f38399246cc6c7e8efe9449dc112ff2f
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-25 01:40:51 +0530

    update to code

--- ticker_extension.php
+++ ticker_extension.php
@@ -1,99 +1,42 @@
 <div id="currency-rate-area">
     <div class="currency-rate-slider">
         <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-up">(+5.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
+        <?php
+        $curl = curl_init();
 
-        <!-- Single Currency Rate Start -->
-        <a  href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-up">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=ETH&convert=EUR",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "Accept: application/json",
+                "Content-Type: application/json",
+                "X-CMC_PRO_API_KEY: dd7eacc4-930e-44b9-8047-a1ddccf2ac82"
+            ),
+        ));
 
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
 
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
+        curl_close($curl);
 
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-up">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
-
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
-
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
-
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
-
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-up">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
-
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
-
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
-
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
-
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-up">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
-
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
-
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-up">(+1.33%)</span></p>
-        </a>
-        <!-- Single Currency Rate End -->
-
-        <!-- Single Currency Rate Start -->
-        <a href="single-airdrop.php" class="single-currency-rate">
-            <p><strong>ETH:</strong>€ 9,953.72 <span class="groth-down">(+1.33%)</span></p>
-        </a>
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $data = json_decode($response, true);
+            $price = $data['data']['ETH']['quote']['EUR']['price'];
+            $change24h = $data['data']['ETH']['quote']['EUR']['percent_change_24h'];
+            $change24hClass = $change24h >= 0 ? 'groth-up' : 'groth-down';
+            echo '<a href="single-airdrop.php" class="single-currency-rate">
+                    <p><strong>ETH:</strong>€ ' . $price . ' <span class="' . $change24hClass . '">(' . $change24h . '%)</span></p>
+                </a>';
+        }
+        ?>
         <!-- Single Currency Rate End -->
     </div>
 </div>
