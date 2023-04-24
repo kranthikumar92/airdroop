ff17af9a4247f5b725cb1d86994a421723b71004
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-24 12:57:08 +0530

    update to code

--- admin/add-user.php
+++ admin/add-user.php
@@ -57,10 +57,10 @@
 
   <!-- Start Page Header -->
   <div class="page-header">
-    <h1 class="title">Add Blog Post</h1>
+    <h1 class="title">Add New User</h1>
       <ol class="breadcrumb">
-        <li><a href="blog-list.php">Airdrop</a></li>
-        <li class="active">Add New Post</li>
+        <li><a href="users.php">Users</a></li>
+        <li class="active">Add User</li>
       </ol>
 
   </div>
