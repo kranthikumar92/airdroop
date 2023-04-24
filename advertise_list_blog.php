158233bdb0111455fb42ce786f50b47ee9be91c1
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-24 23:55:54 +0530

    update to code

--- advertise_list_blog.php
+++ advertise_list_blog.php
@@ -1,7 +1,34 @@
+<!--Retrieve data from db -->
+<?php
+    // Establish a database connection
+    $servername = "localhost";
+    $username = "calix_web_user";
+    $password = "calixworldhhUUh383287HGSHhs";
+    $dbname = "calix_cry_world";
 
+    // Create database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Query to retrieve blog post with ID 1
+$sql = "SELECT * FROM advertisement WHERE ads_location = 'airdrop_desc' AND status = 'Active'";
+$result = mysqli_query($conn, $sql);
+
+// Check if the query returned any rows
+if (mysqli_num_rows($result) > 0) {
+    // Fetch the first row as an associative array
+    $post = mysqli_fetch_assoc($result);
+
+?>
 <div class="col-lg-4">
     <div class="sidebar-advisement d-none d-lg-block">
-        <a href="#"><img src="assets/img/images/slidebar-ads.jpg" alt="Advisement"/></a>
+        <a href="<?php echo $post['banner_link']; ?>"><img src="ads_imgs/<?php echo $post['banner_img']; ?>" alt="<?php echo $post['banner_title']; ?>"/></a>
     </div>
 </div>
+<?php
+}
+?>
