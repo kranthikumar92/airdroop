eed4edc36d45e8aa38278035a5c40de71c0592c4
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-24 14:53:36 +0530

    update to code

--- search-airdrop.php
+++ search-airdrop.php
@@ -203,7 +203,6 @@ $days = $dateInterval->days;
         <?php if ($row_social['website_link']): ?><a href="<?php echo $row_social['website_link']; ?>" target="_blank"><i class="fa fa-chrome"></i></a><?php endif; ?>
         <?php if ($row_social['facebook_link']): ?><a href="<?php echo $row_social['facebook_link']; ?>" target="_blank"><i class="fa fa-facebook-official"></i></a><?php endif; ?>
         <?php if ($row_social['twitter_link']): ?><a href="<?php echo $row_social['twitter_link']; ?>" target="_blank"><i class="fa fa-twitter"></i></a><?php endif; ?>
-        <?php if ($row_social['github_link']): ?><a href="<?php echo $row_social['github_link']; ?>" target="_blank"><i class="fa fa-github"></i></a><?php endif; ?>
         <?php if ($row_social['telegram_link']): ?><a href="<?php echo $row_social['telegram_link']; ?>" target="_blank"><i class="fa fa-telegram"></i></a><?php endif; ?>
     </div>
     <div class="airdrop-pricing-area">
