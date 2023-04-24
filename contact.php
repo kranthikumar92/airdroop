e6fed92fd76076976e64b2a7eb6ad280d278e0d8
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-25 00:14:00 +0530

    update to code

--- contact.php
+++ contact.php
@@ -109,10 +109,7 @@ include('ticker_extension.php');
 
                         <div class="row">
                             <div class="col-lg-2 d-none d-lg-block">
-                                <div class="step-page-adsvisement">
-                                    <a href="#"><img src="assets/img/images/advisement-vertical.jpg" alt="Advisement"
-                                                     class="img-fluid"/></a>
-                                </div>
+                                
                             </div>
 
                             <div class="col-lg-8">
