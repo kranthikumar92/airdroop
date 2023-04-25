4810135499efe9e7be0477e0b760ef7c026fc8b5
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-25 17:14:37 +0530

    update to code

--- admin/login.php
+++ admin/login.php
@@ -17,28 +17,75 @@
   <body>
 
     <div class="login-form">
-      <form action="index.html">
-        <div class="top">
-          <img src="img/kode-icon.png" alt="icon" class="icon">
-          <h1>Kode</h1>
-          <h4>Bootstrap Admin Template</h4>
-        </div>
-        <div class="form-area">
-          <div class="group">
-            <input type="text" class="form-control" placeholder="Username">
-            <i class="fa fa-user"></i>
-          </div>
-          <div class="group">
-            <input type="password" class="form-control" placeholder="Password">
-            <i class="fa fa-key"></i>
-          </div>
-          <div class="checkbox checkbox-primary">
-            <input id="checkbox101" type="checkbox" checked>
-            <label for="checkbox101"> Remember Me</label>
-          </div>
-          <button type="submit" class="btn btn-default btn-block">LOGIN</button>
-        </div>
-      </form>
+    <?php
+// Database connection parameters
+$servername = "localhost";
+$username = "calix_web_user";
+$password = "calixworldhhUUh383287HGSHhs";
+$dbname = "calix_cry_world";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_POST['login'])) {
+  // Retrieve username and password from form data
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $remember = isset($_POST['remember']) ? true : false;
+
+  // Query database to check if user exists and password matches
+  $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password' AND status ='Active'";
+  $result = $conn->query($sql);
+
+  // If user exists and password matches, redirect to index.php
+  if ($result->num_rows > 0) {
+    session_start();
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
+    $_SESSION['login_time'] = time(); // set session login time
+
+    if ($remember) {
+      setcookie('username', $username, time() + (86400 * 30), "/"); // 30 days
+      setcookie('password', $password, time() + (86400 * 30), "/"); // 30 days
+    }
+
+    header("Location: index.php");
+    exit();
+  } else {
+    echo "Invalid username or password";
+  }
+}
+
+$conn->close();
+?>
+<form action="login.php" method="POST">
+  <div class="top">
+    <img src="img/kode-icon.png" alt="icon" class="icon">
+    <h1>Kode</h1>
+    <h4>Bootstrap Admin Template</h4>
+  </div>
+  <div class="form-area">
+    <div class="group">
+      <input type="text" name="username" class="form-control" placeholder="Username">
+      <i class="fa fa-user"></i>
+    </div>
+    <div class="group">
+      <input type="password" name="password" class="form-control" placeholder="Password">
+      <i class="fa fa-key"></i>
+    </div>
+    <div class="checkbox checkbox-primary">
+      <input id="checkbox101" type="checkbox" name="remember" checked>
+      <label for="checkbox101"> Remember Me</label>
+    </div>
+    <button type="submit" name="login" class="btn btn-default btn-block">LOGIN</button>
+  </div>
+</form>
+
       <div class="footer-links row">
         <div class="col-xs-6"><a href="#"><i class="fa fa-external-link"></i> Register Now</a></div>
         <div class="col-xs-6 text-right"><a href="#"><i class="fa fa-lock"></i> Forgot password</a></div>
