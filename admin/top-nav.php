40be3618025eddd3acf0770aa248b650135c9b74
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-25 17:04:36 +0530

    update to code

--- admin/top-nav.php
+++ admin/top-nav.php
@@ -3,7 +3,7 @@
     <li class="dropdown link">
       <a href="#" data-toggle="dropdown" class="dropdown-toggle profilebox"><img src="img/profileimg.png" alt="img"><b>Jonathan Doe</b><span class="caret"></span></a>
         <ul class="dropdown-menu dropdown-menu-list dropdown-menu-right">
-          <li><a href="#"><i class="fa falist fa-user"></i>Users<span class="badge label-danger">4</span></a></li>
+          <li><a href="#"><i class="fa falist fa-user"></i>Users</a></li>
           <li class="divider"></li>
           <li><a href="#"><i class="fa falist fa-power-off"></i> Logout</a></li>
         </ul>
