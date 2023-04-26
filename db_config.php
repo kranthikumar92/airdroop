654cf4e0a3ae2e6f66b07c4b8e6cc474bd7bcdc6
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-27 02:02:08 +0530

    update to code

---
+++ db_config.php
@@ -0,0 +1,16 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "calix_web_user";
+$password = "calixworldhhUUh383287HGSHhs";
+$dbname = "calix_cry_world";
+
+// Create database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
