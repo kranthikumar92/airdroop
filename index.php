1ffb85d2aa7f975ff64703b4e6d2b4eb8bedafda
kranthikumar92 <kranthikumar384@gmail.com>
2023-04-27 01:05:43 +0530

    update to code

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-    <title>Airdroppr - Airdrop Listing Html Template</title>
+    <title>Airdrop Listo - Crypto Airdrop Listing Template</title>
 
-    <meta name="description" content="simple description for your site" />
-<meta name="keywords" content="keyword1, keyword2" />
-<meta name="author" content="ThemeMarshal" />
+<meta name="description" content="Crypto Airdrop Listing Website is your one-stop destination to find and participate in the latest cryptocurrency airdrops. Get access to the hottest and most promising airdrops." />
+<meta name="keywords" content="Crypto Airdrop, Cryptocurrency, Airdrops, Free Coins, Tokens, Crypto Community, Blockchain, ICO, Crypto Rewards, Crypto Giveaways" />
+
 
 <!-- desktop bookmark -->
 <meta name="msapplication-TileColor" content="#ffffff">
