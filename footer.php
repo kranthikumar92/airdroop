b09be52f9d3d5bbe5718d475e473ba0689690bf6
kranthikumar92 <kranthikumar384@gmail.com>
2023-05-04 12:18:29 +0530

    update to code

--- footer.php
+++ footer.php
@@ -94,7 +94,7 @@
 
                             // Create a new AJAX request to insert the email into the database
                             const xhr = new XMLHttpRequest();
-                            xhr.open("POST", "subscribe.php", true);
+                            xhr.open("POST", "subscribe", true);
                             xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
                             xhr.onload = () => {
                                 if (xhr.readyState === 4 && xhr.status === 200) {
