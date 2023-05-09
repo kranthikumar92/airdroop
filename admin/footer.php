c48ffef2221e7a1baf4178026b29e203dc01adfa
kranthikumar92 <kranthikumar384@gmail.com>
2023-05-09 15:14:36 +0530

    update to code

--- admin/footer.php
+++ admin/footer.php
@@ -1,10 +1,10 @@
 <!-- Start Footer -->
 <div class="row footer">
   <div class="col-md-6 text-left">
-  Copyright © 2015 <a href="http://themeforest.net/user/egemem/portfolio" target="_blank">Egemem</a> All rights reserved.
+  Copyright © 2023 <a href="#" target="_blank">Airdrop Listo</a> All rights reserved.
   </div>
   <div class="col-md-6 text-right">
-    Design and Developed by <a href="http://themeforest.net/user/egemem/portfolio" target="_blank">Egemem</a>
+    Design and Developed by <a href="#" target="_blank">Airdrop Listo</a>
   </div> 
 </div>
 <!-- End Footer -->
